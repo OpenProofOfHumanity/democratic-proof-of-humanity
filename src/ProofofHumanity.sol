7a04a4b3e5b6a40ecf833dbc0ed4149d556cc221
Santiago Siri <santisiri@gmail.com>
2022-12-29 17:14:38 +0100

    feat: basic staking contract

--- src/ProofofHumanity.sol
+++ src/ProofofHumanity.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract ProofOfHumanity{}
+error TransferFailed();
+
+contract ProofOfHumanity {
+  mapping (address => mapping(address => uint256)) public balances;
+
+  function stake(uint256 amount, address token) external returns (bool) {
+    balances[msg.sender][token] += amount;
+    bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
+    if (!success) revert TransferFailed();
+    return success;
+  }
+}
