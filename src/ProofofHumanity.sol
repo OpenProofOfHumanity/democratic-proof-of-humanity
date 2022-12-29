0cef5479f52b7b91c9ac06b077a10fd8b97eb203
Santiago Siri <santisiri@gmail.com>
2022-12-29 17:38:42 +0100

    feat: mini contract

--- src/ProofofHumanity.sol
+++ src/ProofofHumanity.sol
@@ -1,61 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "forge-std/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract ProofofHumanity {
+error TransferFailed();
 
-  // Set up the ERC20 token
-  IERC20 private _token;
-  string public name;
-  string public symbol;
-  uint8 public decimals;
-  uint256 public totalSupply;
+contract ProofOfHumanity {
+  mapping (address => mapping(address => uint256)) public balances;
 
-  // Set up the registry
-  struct Human {
-      string name;
-      string description;
-      uint256 applicationDate;
-      uint256 voteBy;
-      bool accepted;
-      uint256 deposit;
-  }
-  mapping(uint256 => Human) public humans;
-  uint256 public humanCount;
-
-  // Set up the parameters for the registry
-  uint256 public minDeposit;
-  uint256 public applyStageLength;
-  uint256 public votingStageLength;
-  uint256 public minQuorum;
-  uint256 public voteQuorum;
-  uint256 public votingPeriodLength;
-
-  // Set up the contract owner and the curator
-  address public owner;
-  address public curator;
-
-  // Set up the events
-  event HumanAdded(uint256 indexed humanId, string name, string description, uint256 deposit);
-  event HumanRemoved(uint256 indexed humanId, string name, string description, uint256 deposit);
-  event HumanAccepted(uint256 indexed humanId, string name, string description, uint256 deposit);
-  event HumanRejected(uint256 indexed humanId, string name, string description, uint256 deposit);
-
-  constructor(
-      address _curator,
-      IERC20 _token
-  ) public {
-      // Set the owner and curator
-      owner = msg.sender;
-      curator = _curator;
-
-      // Set up the ERC20 token
-      _token = _token;
-  }
-
-  // Function to add a human to the registry
-  function addHuman(string memory name, string memory description) public payable {
-      require(msg.value >= minDeposit, "Deposit insufficient");
+  function stake(uint256 amount, address token) external returns (bool) {
+    balances[msg.sender][token] += amount;
+    bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
+    if (!success) revert TransferFailed();
+    return success;
   }
 }
