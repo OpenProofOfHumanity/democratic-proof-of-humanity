f908dcb09614f4010f0c55b3fde08bc747d50414
Santiago Siri <santisiri@gmail.com>
2022-12-31 01:27:51 +0100

    feat: cheat codes for testing

--- test/ProofOfHumanity.t.sol
+++ test/ProofOfHumanity.t.sol
@@ -4,33 +4,23 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/ProofOfHumanity.sol";
 import "./mocks/MockERC20.sol";
+import "./utils/Cheats.sol";
 
 contract TestContract is Test {
+    Cheats internal constant cheats = Cheats(HEVM_ADDRESS);
     ProofOfHumanity public proofOfHumanity;
     MockERC20 public mockToken;
-    ErrorsTest test;
 
     function setUp() public {
         proofOfHumanity = new ProofOfHumanity();
         mockToken = new MockERC20();
-        test = new ErrorsTest();
     }
 
-    function testExample() public {
+    function testStake() public {
       uint256 amount = 10e18;
       mockToken.approve(address(proofOfHumanity), amount);
+      cheats.roll(55);
       bool passed = proofOfHumanity.stake(amount, address(mockToken));
       assertTrue(passed);
-    }
-
-    function testExpectArithmetic() public {
-        vm.expectRevert(stdError.arithmeticError);
-        test.arithmeticError(10);
-    }
-}
-
-contract ErrorsTest {
-    function arithmeticError(uint256 a) public {
-        uint256 a = a - 100;
     }
 }
