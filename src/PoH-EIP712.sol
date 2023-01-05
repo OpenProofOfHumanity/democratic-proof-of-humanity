25a0587b6140090b07d8d4e3fb7302d766efc626
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-05 01:35:27 -0300

    feat: add domain separator

--- src/PoH-EIP712.sol
+++ src/PoH-EIP712.sol
@@ -5,4 +5,8 @@ import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 contract POH_EIP712 is EIP712 {
 	constructor() EIP712("Proof of Humanity", "2") {}
+
+	function DOMAIN_SEPARATOR() external view returns (bytes32) {
+		return _domainSeparatorV4();
+	}
 }
