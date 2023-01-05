7fad8cb9831e357eb74786fce71340b2ab640ec5
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-05 02:18:45 -0300

    add Signature struct

--- src/PoH-EIP712.sol
+++ src/PoH-EIP712.sol
@@ -4,6 +4,12 @@ pragma solidity 0.8.14;
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
+struct Signature {
+	uint8 v;
+	bytes32 r;
+	bytes32 s;
+}
+
 contract POH_EIP712 is EIP712 {
 	bytes32 private constant _HUMANITY_VOUCHER_TYPEHASH =
 		keccak256("HumanityVoucher(address vouchedHuman,uint256 expirationTimestamp)");
