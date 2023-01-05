82027c56cf7e263aa07dec51fbfc45c08bd7a7f7
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-05 01:34:30 -0300

    init EIP712 contract

--- src/PoH-EIP712.sol
+++ src/PoH-EIP712.sol
@@ -2,3 +2,7 @@
 pragma solidity 0.8.14;
 
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+
+contract POH_EIP712 is EIP712 {
+	constructor() EIP712("Proof of Humanity", "2") {}
+}
