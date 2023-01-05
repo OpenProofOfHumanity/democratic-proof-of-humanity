9a90a2d5914751f0663e89500b6f5d04029f1520
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-05 01:57:40 -0300

    add humanity voucher typehash

--- src/PoH-EIP712.sol
+++ src/PoH-EIP712.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.14;
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 contract POH_EIP712 is EIP712 {
+	bytes32 private constant _HUMANITY_VOUCHER_TYPEHASH =
+		keccak256("HumanityVoucher(address vouchedHuman,uint256 expirationTimestamp)");
+
 	constructor() EIP712("Proof of Humanity", "2") {}
 
 	function DOMAIN_SEPARATOR() external view returns (bytes32) {
