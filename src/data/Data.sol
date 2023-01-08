8292f92c80b6df3c0297841f864f0dee5b4e5a6d
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-07 22:13:30 -0300

    add humanity status

--- src/data/Data.sol
+++ src/data/Data.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import {HumanityStatus} from "../data-structures/HumanityStatus.sol";
 import {IData} from "./IData.sol";
 
-contract Data is IData {}
+contract Data is IData {
+	mapping(uint256 => HumanityStatus) private _status;
+
+	function statusOf(uint256 humanId) external view returns (HumanityStatus) {
+		return _status[humanId];
+	}
+}
--- src/data/IData.sol
+++ src/data/IData.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-interface IData {}
+import {HumanityStatus} from "../data-structures/HumanityStatus.sol";
+
+interface IData {
+	function statusOf(uint256 humanId) external view returns (HumanityStatus);
+}
