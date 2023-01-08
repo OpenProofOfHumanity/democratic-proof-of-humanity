47acf858e21d3f6e5e62fd7a1291dcb2dfc27349
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-07 22:26:49 -0300

    feat(SBT): token minting

--- src/sbt/SBT.sol
+++ src/sbt/SBT.sol
@@ -5,4 +5,9 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SBT is ERC721 {
 	constructor() ERC721("Humanity Id", "HUMAN") {}
+
+	function mint(uint256 humanId, address human) external {
+		// pending: check msg.sender
+		_safeMint(human, humanId);
+	}
 }
