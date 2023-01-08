01327d6a05e130ae870c0d400907cc5bb32f634a
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-07 22:33:17 -0300

    feat(SBT): define transfer

--- src/sbt/ISBT.sol
+++ src/sbt/ISBT.sol
@@ -3,4 +3,8 @@ pragma solidity 0.8.17;
 
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-interface ISBT is IERC721 {}
+interface ISBT is IERC721 {
+	function mint(uint256 humanId, address initialAddress) external;
+
+	function transfer(uint256 humanId, address newAddress) external;
+}
--- src/sbt/SBT.sol
+++ src/sbt/SBT.sol
@@ -2,12 +2,18 @@
 pragma solidity 0.8.17;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ISBT} from "./ISBT.sol";
 
-contract SBT is ERC721 {
+contract SBT is ISBT, ERC721 {
 	constructor() ERC721("Humanity Id", "HUMAN") {}
 
-	function mint(uint256 humanId, address human) external {
-		// pending: check msg.sender
-		_safeMint(human, humanId);
+	function mint(uint256 humanId, address initialAddress) external {
+		// pending: check msg.sender -> registration contract
+		_safeMint(initialAddress, humanId);
+	}
+
+	function transfer(uint256 humanId, address newAddress) external {
+		// pending: check msg.sender -> recovery contract
+		_safeTransfer(ownerOf(humanId), newAddress, humanId, "");
 	}
 }
