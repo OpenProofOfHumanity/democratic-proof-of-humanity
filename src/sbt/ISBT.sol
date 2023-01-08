a89aa307b45cb84cfb10e98a680cdc088358c1f8
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-07 22:24:29 -0300

    create SBT contracts

--- src/sbt/ISBT.sol
+++ src/sbt/ISBT.sol
@@ -1,2 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+interface ISBT is IERC721 {}
--- src/sbt/SBT.sol
+++ src/sbt/SBT.sol
@@ -1,2 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SBT is ERC721 {
+	constructor() ERC721("Humanity Id", "HUMAN") {}
+}
