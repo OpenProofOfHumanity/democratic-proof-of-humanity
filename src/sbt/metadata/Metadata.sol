bd880bccea214ea9d13783add99585c610fe40b3
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-07 22:43:20 -0300

    feat(Metadata): add metadata v1

--- src/sbt/metadata/Metadata.sol
+++ src/sbt/metadata/Metadata.sol
@@ -1,6 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {IMetadata} from "./IMetadata.sol";
 
-contract Metadata is IMetadata {}
+contract Metadata is IMetadata {
+	using Strings for uint256;
+
+	string private _baseURI;
+
+	function tokenURI(uint256 humanId) external view returns (string memory) {
+		return string(abi.encodePacked(_baseURI, humanId.toString(), ".json"));
+	}
+
+	function contractURI() external view returns (string memory) {
+		return string(abi.encodePacked(_baseURI, "collection.json"));
+	}
+
+	function updateBaseURI(string calldata newBaseURI) external {
+		// pending: check msg.sender -> governor
+		_baseURI = newBaseURI;
+	}
+}
