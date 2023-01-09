a3189e99bf264dfea2d58fda80fa73db81fa2946
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-09 18:54:59 -0300

    feat: add RequestType

--- src/verification/phases/IVerificationPhase.sol
+++ src/verification/phases/IVerificationPhase.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import {RequestType} from "../../data-structures/RequestType.sol";
+
 interface IVerificationPhase {
-	function complete(uint256 requestId) external returns (bool);
+	function complete(RequestType requestType, uint256 requestId) external returns (bool);
 }
