5f33ab5f87b40251a2d4f6554db34b789fb5dce7
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-09 19:10:56 -0300

    feat: add NewStatus event

--- src/verification/processes/VerificationProcess.sol
+++ src/verification/processes/VerificationProcess.sol
@@ -3,11 +3,12 @@ pragma solidity 0.8.17;
 
 import {IVerificationProcess} from "./IVerificationProcess.sol";
 import {RequestStatus} from "../../data-structures/RequestStatus.sol";
+import {RequestData} from "../../data-structures/RequestData.sol";
 
 abstract contract VerificationProcess is IVerificationProcess {
 	event NewRequest(uint256 requestId, uint256 requester, string evidence);
 
-	event NewPhase(uint256 requestId, );
+	event NewPhase(uint256 requestId, RequestStatus newStatus);
 
 	function necessaryVouches() external view virtual returns (uint256 amount);
 
