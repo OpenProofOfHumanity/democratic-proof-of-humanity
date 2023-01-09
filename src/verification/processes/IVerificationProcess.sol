8c4bbe151250560c55782517bf828292b30f36f4
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-09 18:45:35 -0300

    feat(VerificationProcess): define interface

--- src/verification/processes/IVerificationProcess.sol
+++ src/verification/processes/IVerificationProcess.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-interface IVerificationProcess {}
+import {RequestStatus} from "../../data-structures/RequestStatus.sol";
+
+interface IVerificationProcess {
+	function necessaryVouches() external view returns (uint256 amount);
+
+	function necessaryFunding() external view returns (address token, uint256 amount);
+
+	function requestStatus(uint256 requestId) external view returns (RequestStatus);
+}
