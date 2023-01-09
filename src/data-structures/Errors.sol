5f1f44d67b2431941c6d1b9726868e01f5559ce7
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-09 19:50:46 -0300

    style: organize errors

--- src/data-structures/Errors.sol
+++ src/data-structures/Errors.sol
@@ -3,18 +3,25 @@ pragma solidity 0.8.17;
 
 import {RequestStatus} from "./RequestStatus.sol";
 
+// general
+
+// humanity
+
 error NotHumanID(uint256 humanId);
 error NotHumanAddress(address human);
 
-error AlreadyHuman(address human);
-error AlreadySubmitted(address submitter);
+// transferability
 
 error UnauthorizedTransfer();
+
+// phases completion
 
 error IncompleteVouching(uint256 requestId);
 error IncompleteFunding(uint256 requestId);
 error IncompleteConfirmation(uint256 requestId);
 
-error InvalidCurrentStatus(uint256 requestId, RequestStatus current, RequestStatus expected);
+// process checks
 
+error AlreadyHuman(address human);
 error AnotherPendingRequest(address requester);
+error InvalidCurrentStatus(uint256 requestId, RequestStatus current, RequestStatus expected);
